01dffea7a904a5d8a8da7c40411d6a4c1d2d590c
Radhi Rasho <Radhi.h@outlook.com>
2024-02-06 17:10:32 +0000

    updated versions

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,21 @@ import BestPractices from "./best-practices";
 import TProxy from "./proxy";
 import JWTAuth from "./jwt-auth";
 import Streaming from "./streaming";
+import { timing } from "hono/timing";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
 
 const app = new Hono();
 
+app.use("*", timing({ enabled: (c) => c.req.method === "GET" }));
+app.use("*", logger());
+app.use("*", prettyJSON());
 app.route("/routes", Routes);
 app.route("/context", Context);
 app.route("/request", HonoReq);
+
 // app.route("/ws", Socket);
+
 app.route("/middleware", MiddleWare);
 app.route("/helpers", Helpers);
 app.route("/jsx", JSX);
@@ -32,10 +40,10 @@ app.route("/proxy", TProxy);
 app.route("/jwt-auth", JWTAuth);
 app.route("/stream", Streaming);
 
-// console.log("======c Get Router Name =======");
-// console.log(getRouterName(app));
+//! console.log("======c Get Router Name =======");
+//! console.log(getRouterName(app));
 
-// console.log("========= Get Routes =========");
-// showRoutes(app);
+//! console.log("========= Get Routes =========");
+//! console.log(showRoutes(app));
 
 export default app;
